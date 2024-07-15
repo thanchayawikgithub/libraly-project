42d12e1b220458bf57c3a071c0df1199430ad15c
thanchayawik <thanchayawik@gmail.com>
2024-07-15 13:11:01 +0700

    set up config (env)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSource } from 'typeorm';
 import { Book } from './books/entities/book.entity';
 import { BooksModule } from './books/books.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env.local'], isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'rootpassword',
-      database: 'library_db',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Book],
       synchronize: true,
     }),
